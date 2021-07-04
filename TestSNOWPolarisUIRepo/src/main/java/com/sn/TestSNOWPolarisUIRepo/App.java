dba57b7856934f519a00a21f1bc6c8f805a75ad3
Admin McAdmin <test@dummy.com>
2021-07-05 01:03:26 +0530

    updating story

--- TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
+++ TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Commit 1 : update story" );
+        
+        System.out.println( "Commit 2 : update story" );
     }
 }
