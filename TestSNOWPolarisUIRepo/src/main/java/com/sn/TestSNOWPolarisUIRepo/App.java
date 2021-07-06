bc19c5cd2517a4d0bd7adc43e643bb1858206def
Admin McAdmin <test@dummy.com>
2021-07-06 20:12:36 +0530

    STRY0010178: story update

--- TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
+++ TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
@@ -25,5 +25,7 @@ public class App
        System.out.println("STRY0010054: Story update");
        
        System.out.println("STRY0010005: Story update");
+       
+       System.out.println("STRY0010178: Story update");
     }
 }
