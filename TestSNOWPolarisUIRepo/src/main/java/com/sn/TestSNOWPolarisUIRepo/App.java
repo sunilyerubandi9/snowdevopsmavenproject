beecddbd51f706b13980c2a7703aa53feb12bf1b
Admin McAdmin <test@dummy.com>
2021-07-08 21:13:11 +0530

    725: Story update

--- TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
+++ TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
@@ -33,5 +33,8 @@ public class App
        System.out.println("725: Story update");
        
        System.out.println("723: Story update");
+       
+       System.out.println("725: Story update");
     }
+    
 }
