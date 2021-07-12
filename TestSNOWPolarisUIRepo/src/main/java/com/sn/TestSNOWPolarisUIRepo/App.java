5c6f7a6f8d010a10eeda4785c587062847db6ffa
Admin McAdmin <test@dummy.com>
2021-07-12 19:30:43 +0530

    723: Story update

--- TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
+++ TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
@@ -54,6 +54,8 @@ public class App
        System.out.println("723: Story update");
        
        System.out.println("725: Story update");
+       
+       System.out.println("723: Story update");
     }
     
 }
