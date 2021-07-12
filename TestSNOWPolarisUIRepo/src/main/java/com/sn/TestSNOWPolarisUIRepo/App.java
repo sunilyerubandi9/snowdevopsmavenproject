047d00269e4edc9c062662ee04bc71c8ed6b98c4
Admin McAdmin <test@dummy.com>
2021-07-12 19:48:38 +0530

    725: Story update

--- TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
+++ TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
@@ -8,54 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Commit 1 : update story" );
-        
-        System.out.println( "Commit 2 : update story" );
-        System.out.println( "Commit 3 : update story" );
-        System.out.println( "Commit 4 : update story" );
-        
-        System.out.println( "Commit 5 : update story" );
-        
-        System.out.println( "Commit 6 : update story" );
-        
-       System.out.println("STRY0010053: Story update");
-       
-       System.out.println("STRY0010053: Story update");
-       
-       System.out.println("STRY0010054: Story update");
-       
-       System.out.println("STRY0010005: Story update");
-       
-       System.out.println("STRY0010178: Story update");
-       
-       System.out.println("718: Story update");
-       
-       System.out.println("725: Story update");
-       
-       System.out.println("723: Story update");
-       
-       System.out.println("725: Story update");
-       System.out.println("720: Story update");
-       
-       System.out.println("725: Story update");
-       
-       System.out.println("723: Story update");
-       
-       System.out.println("720: Story update");
-       
-       System.out.println("725: Story update");
-       
-       System.out.println("723: Story update");
-       
-       System.out.println("720: Story update");
-       
-       System.out.println("725: Story update");
-       
-       System.out.println("723: Story update");
-       
-       System.out.println("725: Story update");
-       
-       System.out.println("723: Story update");
        
        System.out.println("725: Story update");
     }
