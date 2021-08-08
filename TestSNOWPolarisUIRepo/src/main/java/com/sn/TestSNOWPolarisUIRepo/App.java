843819fca6603065642a7ce0a318fa3e460803cc
Admin McAdmin <test@dummy.com>
2021-08-09 01:27:33 +0530

    STRY0010035: Story update

--- TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
+++ TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
@@ -7,6 +7,6 @@ package com.sn.TestSNOWPolarisUIRepo;
 public class App {
     public static void main( String[] args ){
     	
-       System.out.println("STRY0010101: Story update");
+       System.out.println("	STRY0010035: Story update");
     }
 }
