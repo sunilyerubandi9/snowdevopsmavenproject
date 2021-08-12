6c111b962d2d69478974a57d879840c9f6fa9da2
Admin McAdmin <test@dummy.com>
2021-08-12 18:53:13 +0530

    737: Bug update

--- TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
+++ TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
@@ -7,6 +7,6 @@ package com.sn.TestSNOWPolarisUIRepo;
 public class App {
     public static void main( String[] args ){
     	
-       System.out.println("736: Feature update");
+       System.out.println("737: Bug update");
     }
 }
