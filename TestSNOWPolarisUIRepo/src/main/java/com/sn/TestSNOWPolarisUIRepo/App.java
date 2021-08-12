4ffb9865d7200accb2eab4ee56c3af15c0955d1d
Admin McAdmin <test@dummy.com>
2021-08-12 21:16:58 +0530

    741: Issue update

--- TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
+++ TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
@@ -7,6 +7,6 @@ package com.sn.TestSNOWPolarisUIRepo;
 public class App {
     public static void main( String[] args ){
     	
-       System.out.println("740: Issue update ");
+       System.out.println("741: Issue update ");
     }
 }
