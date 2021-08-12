390d1cf2a5be1a554999feaa0700a910c91149e4
Admin McAdmin <test@dummy.com>
2021-08-12 21:27:50 +0530

    745: Story update

--- TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
+++ TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
@@ -7,6 +7,6 @@ package com.sn.TestSNOWPolarisUIRepo;
 public class App {
     public static void main( String[] args ){
     	
-       System.out.println("742: Issue update ");
+       System.out.println("745: Issue update ");
     }
 }
