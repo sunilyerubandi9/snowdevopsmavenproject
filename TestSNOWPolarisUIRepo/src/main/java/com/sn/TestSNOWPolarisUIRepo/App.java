972aeec9f06c8c252ec0e8294244ed1e060cd334
Admin McAdmin <test@dummy.com>
2021-08-19 00:31:00 +0530

    733: Story 733 update

--- TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
+++ TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
@@ -7,6 +7,6 @@ package com.sn.TestSNOWPolarisUIRepo;
 public class App {
     public static void main( String[] args ){
     	
-       System.out.println("732: Story 732 update");
-    }
+       System.out.println("733: Story 733 update");
+    
 }
