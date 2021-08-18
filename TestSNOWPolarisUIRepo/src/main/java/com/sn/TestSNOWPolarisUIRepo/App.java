1d1fb83bcf63abd8aea80e1749ec82e9512b38f5
Admin McAdmin <test@dummy.com>
2021-08-19 00:35:37 +0530

    734: Story 734 update

--- TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
+++ TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
@@ -7,6 +7,6 @@ package com.sn.TestSNOWPolarisUIRepo;
 public class App {
     public static void main( String[] args ){
     	
-       System.out.println("733: Story 733 update");
+       System.out.println("734: Story 734 update");
     
 }
