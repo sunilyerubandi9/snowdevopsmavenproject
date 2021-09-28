d80fb9a24722351cc91febb9656f4f9f526ee471
sunil.yerubandi <sunil.yerubandi@servicenow.com>
2021-09-28 18:14:24 +0530

    Adding Tests

--- TestSNOWPolarisUIRepo/src/test/java/com/sn/TestSNOWPolarisUIRepo/AppTestPassed.java
+++ TestSNOWPolarisUIRepo/src/test/java/com/sn/TestSNOWPolarisUIRepo/AppTestPassed.java
@@ -7,14 +7,13 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest-Passed
+public class AppTestPassed  
 {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue() {
         //new change
         //new change for Cascade Delete
         //Another change for Cascade Delete
