16a90678f6a9b8a8dd6475e40eadc19135cfdc37
sunil.yerubandi <sunil.yerubandi@servicenow.com>
2021-09-28 19:48:03 +0530

    STRY0010026: Story update

--- TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
+++ TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
@@ -7,7 +7,7 @@ package com.sn.TestSNOWPolarisUIRepo;
 public class App {
     public static void main( String[] args )	{
     	
-       System.out.println("STRY0010035: Story update");
+       System.out.println("STRY0010026: Story update");
     
     }
 
