29d04939a0e6b4a6b7f68a2edbc4f93e75ba9fec
sunil.yerubandi <sunil.yerubandi@servicenow.com>
2021-10-07 22:45:27 +0530

    STRY0010143: Story update

--- TestSNOWPolarisUIRepo/src/test/java/com/sn/TestSNOWPolarisUIRepo/TestDevOpsApp03.java
+++ TestSNOWPolarisUIRepo/src/test/java/com/sn/TestSNOWPolarisUIRepo/TestDevOpsApp03.java
@@ -20,6 +20,6 @@ public class TestDevOpsApp03
         //Third Change
         //Change for Cascade Delete, again
         //Change For validation of PipelineUI Tests
-        assertTrue( true );
+        assertTrue( false );
     }
 }
