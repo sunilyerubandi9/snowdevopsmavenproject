394008f6123953c81a1c44cfceaa5a55fb56bedc
sunilyerubandi9 <sunilyerubandi9@gmail.com>
2021-11-18 22:43:03 +0530

    775: Story update

--- TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
+++ TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
@@ -7,7 +7,7 @@ package com.sn.TestSNOWPolarisUIRepo;
 public class App {
     public static void main( String[] args )	{
     	
-       System.out.println("774: Story update");
+       System.out.println("775: Story update");
     
     }
 
