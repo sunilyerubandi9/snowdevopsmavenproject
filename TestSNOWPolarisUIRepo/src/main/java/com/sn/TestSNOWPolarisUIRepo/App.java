48d0a94829dadc6e6382c88c604185fa26178503
sunilyerubandi9 <sunilyerubandi9@gmail.com>
2021-11-18 23:56:22 +0530

    776: Story State has been updated

--- TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
+++ TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
@@ -7,7 +7,7 @@ package com.sn.TestSNOWPolarisUIRepo;
 public class App {
     public static void main( String[] args )	{
     	
-       System.out.println("775: Story State has been updated");
+       System.out.println("776: Story State has been updated");
     
     }
 
