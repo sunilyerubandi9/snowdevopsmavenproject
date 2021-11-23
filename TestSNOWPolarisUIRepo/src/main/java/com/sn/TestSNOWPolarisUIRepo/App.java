9907faec71c93c170f58d19bfeda9a5a9828d935
sunilyerubandi9 <sunilyerubandi9@gmail.com>
2021-11-23 22:15:12 +0530

    796: Story state has been updated

--- TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
+++ TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
@@ -7,7 +7,7 @@ package com.sn.TestSNOWPolarisUIRepo;
 public class App {
     public static void main( String[] args )	{
     	
-       System.out.println("796: Story state has been created");
+       System.out.println("796: Story state has been updated");
     
     }
 
