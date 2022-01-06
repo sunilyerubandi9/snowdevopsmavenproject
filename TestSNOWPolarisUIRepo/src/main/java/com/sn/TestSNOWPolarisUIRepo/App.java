b85c6216fb0cc9df6fe2d5f24933f9dacf22e661
sunilyerubandi9 <85546346+sunilyerubandi9@users.noreply.github.com>
2022-01-06 12:14:51 +0530

    "798: Story state has been updated"
    "798: Story state has been updated"

--- TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
+++ TestSNOWPolarisUIRepo/src/main/java/com/sn/TestSNOWPolarisUIRepo/App.java
@@ -14,6 +14,7 @@ public class App {
        System.out.println("798: Story state has been created");
         System.out.println("798: Story state has been updated");
          System.out.println("798: Story state has been updated");
+        System.out.println("798: Story state has been updated");
     }
 
 }
